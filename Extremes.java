094c977ab66e82d439c9e163cdf4bd256e617800
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 16:03:23 -0500

    Update Extremes.java

--- Extremes.java
+++ Extremes.java
@@ -7,7 +7,7 @@
 * and outputs the minimum, maximum, and average values.
 * The user can stop input by entering a zero.
 *
-**********************************************************/
+***********************************************************/
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
